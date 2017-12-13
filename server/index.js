e15561742e8f95495fb460ff495827b87802a3d6
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-13 21:48:03 +0800

    Forget to add body parser

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import bodyparser from 'body-parser'
 
 import api from './api'
 
@@ -7,6 +8,11 @@ const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
 
 app.use('/api', api)
 
