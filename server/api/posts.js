562373c89d09fef1bf1c3ebd073271ea7ce906f1
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-13 22:11:02 +0800

    finish the post api

--- server/api/posts.js
+++ server/api/posts.js
@@ -2,12 +2,74 @@ import { Router } from 'express'
 
 const router = Router()
 
-const mockPost = [
-  { post: 'Hello World' }
-]
+const PostModel = require('../../models/post.js')
 
 router.get('/posts', (req, res, next) => {
-  res.json(mockPost)
+  PostModel.find({}, (err, posts) => {
+    if (err) {
+      res.json({error: err.name}, 500)
+    }
+    res.json({posts: posts})
+  })
+})
+
+router.get('/post/:id', (req, res, next) => {
+  var id = req.params.id
+  PostModel.findById(id, (err, post) => {
+    if (err) {
+      res.json({error: err.name}, 500)
+    }
+    res.json({post: post})
+  })
+})
+
+router.post('/post', (req, res, next) => {
+  var data = req.body
+  var post = new PostModel(data)
+  post.save((err, result) => {
+    if (err) {
+      res.json({error: err.name}, 500)
+    }
+    res.json({post: result})
+  })
+})
+
+router.put('/post/:id', (req, res, next) => {
+  var id = req.params.id
+  var data = req.body
+  PostModel.findById(id, (err, post) => {
+    if (err) {
+      res.json({error: err.name}, 500)
+    }
+    post.title = data.title
+    post.author = data.author
+    post.content = data.content
+    post.created = data.created
+    post.updated = Date.now()
+
+    post.save((err, result) => {
+      if (err) {
+        res.json({error: err.name}, 500)
+      }
+      res.json({post: result})
+    })
+  })
+})
+
+router.delete('/post/:id', (req, res, next) => {
+  var id = req.params.id
+  PostModel.findById(id, (err, post) => {
+    if (err) {
+      res.json({error: err.name}, 500)
+    }
+
+    post.remove((err, result) => {
+      if (err) {
+        res.json({error: err.name}, 500)
+      }
+      res.json(result)
+    })
+  })
 })
 
 export default router
