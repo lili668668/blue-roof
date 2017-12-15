6f779b9c8d503fee1d6c96b87ecb39d9f45284d5
Tzu-Yin Hong <lili668668@gmail.com>
2017-12-15 08:29:01 +0800

    Allow CORS

--- server/index.js
+++ server/index.js
@@ -14,6 +14,12 @@ app.use(bodyparser.urlencoded({
   extended: true
 }))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use('/api', api)
 
 app.listen(port, host)
